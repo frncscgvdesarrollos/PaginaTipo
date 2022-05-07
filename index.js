b5381b996def91d93067e24fec22a6869f88f50a
Francisco <104695718+frncscgvdesarrollos@users.noreply.github.com>
2022-05-06 21:25:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu");
+const menubarLinkP = document.querySelector(".menubar-link-p");
+
+    menu.addEventListener("click", function() 
+    { 
+        menubarLinkP.classList.toggle("displaynone")
+
+    });
+
